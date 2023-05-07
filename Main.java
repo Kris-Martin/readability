e3b2a0194a5c4b7c1f48cc19c531d989796b4af4
Kris Martin <28503716+Kris-Martin@users.noreply.github.com>
2023-05-07 22:43:14 +0930

    Use try-resources

--- Main.java
+++ Main.java
@@ -1,17 +1,44 @@
 package readability;
 
-import java.util.List;
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.Scanner;
+
 
 public class Main {
 
     public static void main(String[] args) {
-        String input = new Scanner(System.in).nextLine().trim();
-        List<String> sentences = List.of(input.split("[.?!]"));
-        int averageWords = sentences.stream()
-                                    .map(sentence -> sentence.split(" ").length)
-                                    .reduce(0, Integer::sum) / sentences.size();
+        String text = "";
 
-        System.out.print(averageWords > 10 ? "HARD" : "EASY");
+        try (Scanner scanner = new Scanner(new File(args[0]))) {
+            text = scanner.nextLine().trim();
+
+        } catch (FileNotFoundException e) {
+            System.out.println(e.getMessage());
+        }
+
+        int sentences = text.split("[!?.]").length;
+        int words = text.split(" ").length;
+        int characters = text.replaceAll("\\s+", "").length();
+
+        double score = 4.71 * ((double) characters / words) + 0.5 * ((double) words / sentences) - 21.43;
+        int rank = (int) Math.ceil(score);
+        int lowerAge = rank + 4;
+        int upperAge = lowerAge != 18 ? lowerAge + 1 : 24;
+        String ageRange = "%s-%s".formatted(lowerAge, upperAge);
+
+        String output =
+        """
+        The text is:
+        %s
+        
+        Words: %d
+        Sentences: %d
+        Characters: %d
+        The score is: %f
+        This text should be understood by %s year-olds.
+        """.formatted(text, words, sentences, characters, score, ageRange);
+
+        System.out.println(output);
     }
 }
