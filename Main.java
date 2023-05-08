9c84778d0c95bdce7a55aa6b3807daa3eb46b0f4
Kris Martin <28503716+Kris-Martin@users.noreply.github.com>
2023-05-08 23:56:20 +0930

    Refactor to use Text and Readability classes

--- Main.java
+++ Main.java
@@ -6,39 +6,37 @@ import java.util.Scanner;
 
 
 public class Main {
-
     public static void main(String[] args) {
-        String text = "";
 
+        // args[0] for command line, "~/test1.txt" for test
         try (Scanner scanner = new Scanner(new File(args[0]))) {
-            text = scanner.nextLine().trim();
+            String input = scanner.nextLine().trim();
+            Text text = new Text(input);
+            Readability readability = new Readability(text);
+            double ariScore = readability.getARI();
+
+            String output =
+                    """
+                    The text is:
+                    %s
+                                        
+                    Words: %d
+                    Sentences: %d
+                    Characters: %d
+                    The score is: %f
+                    This text should be understood by %s year-olds.
+                    """.formatted(
+                            text.input(),
+                            text.wordCount(),
+                            text.sentenceCount(),
+                            text.charCount(),
+                            ariScore,
+                            readability.getAgeRange(ariScore));
+
+            System.out.println(output);
 
         } catch (FileNotFoundException e) {
             System.out.println(e.getMessage());
         }
-
-        int sentences = text.split("[!?.]").length;
-        int words = text.split(" ").length;
-        int characters = text.replaceAll("\\s+", "").length();
-
-        double score = 4.71 * ((double) characters / words) + 0.5 * ((double) words / sentences) - 21.43;
-        int rank = (int) Math.ceil(score);
-        int lowerAge = rank + 4;
-        int upperAge = lowerAge != 18 ? lowerAge + 1 : 24;
-        String ageRange = "%s-%s".formatted(lowerAge, upperAge);
-
-        String output =
-        """
-        The text is:
-        %s
-        
-        Words: %d
-        Sentences: %d
-        Characters: %d
-        The score is: %f
-        This text should be understood by %s year-olds.
-        """.formatted(text, words, sentences, characters, score, ageRange);
-
-        System.out.println(output);
     }
 }
