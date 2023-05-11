0f911c8cc443c2fe43e9223427ad3db385e4a64d
Kris Martin <28503716+Kris-Martin@users.noreply.github.com>
2023-05-11 20:34:42 +0930

    Refactor record to class

--- Readability.java
+++ Readability.java
@@ -2,8 +2,13 @@ package readability;
 
 public record Readability(Text text) {
     public double getARI() {
-        return 4.71 * ((double) text.charCount() / text().wordCount()) +
-               0.5 * ((double) text.wordCount() / text.sentenceCount()) - 21.43;
+        return 4.71 * ((double) text.getCharCount() / text.getWordCount()) +
+               0.5 * ((double) text.getWordCount() / text.getSentenceCount()) - 21.43;
+    }
+
+    public double getFleschKincaid() {
+        return 0.39 * ((double) text.getWordCount() / text.getSentenceCount()) +
+               11.8 * ((double) text.getTotalSyllables() / text.getWordCount()) - 15.59;
     }
 
     public String getAgeRange(double score) {
--- Text.java
+++ Text.java
@@ -1,15 +1,91 @@
 package readability;
 
-public record Text(String input) {
-    public int charCount() {
+
+public class Text {
+    String input;
+    String[] words;
+    private final int sentenceCount;
+    private final int charCount;
+    private final int totalSyllables;
+    private final int wordCount;
+
+    public Text(String input) {
+        this.input = input;
+        words = setWords();
+        sentenceCount = setSentenceCount();
+        charCount = setCharCount();
+        totalSyllables = setTotalSyllables();
+        wordCount = setWordCount();
+    }
+
+    public static boolean isVowel(char c) {
+        final String vowels = "aeiouy";
+        c = Character.toLowerCase(c);
+        return vowels.contains(String.valueOf(c));
+    }
+
+    public static int syllablesPerWord(String word) {
+        char prev = ' ';
+        char curr;
+        int count = 0;
+        for (int i = 0; i < word.length(); i++) {
+            curr = word.charAt(i);
+            if (isVowel(curr) && !isVowel(prev)) {
+                count++;
+            }
+            if (curr == 'e' && i == word.length() - 1) {
+                count--;
+            }
+            prev = curr;
+        }
+        return count > 0 ? count : 1;
+    }
+
+    private int setCharCount() {
         return input.replaceAll("\\s+", "").length();
     }
 
-    public int wordCount() {
-        return input.split(" ").length;
+    private int setTotalSyllables() {
+        int count = 0;
+        for (String word : this.getWords()) {
+            count += syllablesPerWord(word);
+        }
+        return count;
     }
 
-    public int sentenceCount() {
+    private String[] setWords() {
+        return input.replaceAll("[!?.]", "").split(" ");
+    }
+
+    private int setWordCount() {
+        return this.getWords().length;
+    }
+
+    public String[] getWords() {
+        return words;
+    }
+
+    private int setSentenceCount() {
         return input.split("[!?.]").length;
+    }
+
+    public String getInput() {
+        return input;
+    }
+
+    public int getSentenceCount() {
+        return sentenceCount;
+    }
+
+    public int getCharCount() {
+        return charCount;
+    }
+
+    public int getTotalSyllables() {
+        return totalSyllables;
+    }
+
+    public int getWordCount() {
+        return wordCount;
     }
 }
