9822e3e3b41b2eeda0311666905a787cb6d0fc84
Kris Martin <28503716+Kris-Martin@users.noreply.github.com>
2023-05-12 00:48:04 +0930

    Refactor to use abstract method

--- Indexes.java
+++ Indexes.java
@@ -3,11 +3,35 @@ package readability;
 import java.util.List;
 
 public enum Indexes {
-    ARI("ARI","Automated Readability Index"),
-    FK("FK","Flesch–Kincaid readability tests"),
-    SMOG("SMOG", "Simple Measure of Gobbledygook"),
-    CL("CL", "Coleman–Liau index"),
-    ALL("all", "all");
+    ARI("ARI","Automated Readability Index") {
+        @Override
+        public double getScore(Text text) {
+            return 4.71 * ((double) text.getCharCount() / text.getWordCount()) +
+                   0.5 * ((double) text.getWordCount() / text.getSentenceCount()) - 21.43;
+        }
+    },
+    FK("FK","Flesch–Kincaid readability tests") {
+        @Override
+        public double getScore(Text text) {
+            return 0.39 * ((double) text.getWordCount() / text.getSentenceCount()) +
+                   11.8 * ((double) text.getTotalSyllables() / text.getWordCount()) - 15.59;
+        }
+    },
+    SMOG("SMOG", "Simple Measure of Gobbledygook") {
+        @Override
+        public double getScore(Text text) {
+            return 1.043 * Math.sqrt(text.getPolySyllables() * (30.0 / text.getSentenceCount())) + 3.1291;
+        }
+    },
+    CL("CL", "Coleman–Liau index") {
+        @Override
+        public double getScore(Text text) {
+            double avgChars100Words = (double) text.getCharCount() / text.getWordCount() * 100;
+            double avgSentences100Words = (double) text.getSentenceCount() / text.getWordCount() * 100;
+
+            return 0.0588 * avgChars100Words - 0.296 * avgSentences100Words - 15.8;
+        }
+    };
 
     private final String abbrev;
     private final String name;
@@ -25,29 +49,14 @@ public enum Indexes {
         return name;
     }
 
-    public static double getARI(Text text) {
-        return 4.71 * ((double) text.getCharCount() / text.getWordCount()) +
-                0.5 * ((double) text.getWordCount() / text.getSentenceCount()) - 21.43;
+    public abstract double getScore(Text text);
+
+    public void printOutput(Text text) {
+        System.out.printf("%s: %.2f (about %d-year-olds).%n",  name, getScore(text), getUpperAgeLevel(text));
     }
 
-    public static double getFleschKincaid(Text text) {
-        return 0.39 * ((double) text.getWordCount() / text.getSentenceCount()) +
-                11.8 * ((double) text.getTotalSyllables() / text.getWordCount()) - 15.59;
-    }
-
-    public static double getSMOG(Text text) {
-        return 1.043 * Math.sqrt(text.getPolySyllables() * (30.0 / text.getSentenceCount())) + 3.1291;
-    }
-
-    public static double getColemanLiau(Text text) {
-        double avgChars100Words = (double) text.getCharCount() / text.getWordCount() * 100;
-        double avgSentences100Words = (double) text.getSentenceCount() / text.getWordCount() * 100;
-
-        return 0.0588 * avgChars100Words - 0.296 * avgSentences100Words - 15.8;
-    }
-
-    public static int getUpperAgeLevel(double score) {
-        int rank = (int) Math.ceil(score);
+    public int getUpperAgeLevel(Text text) {
+        int rank = (int) Math.ceil(getScore(text));
         int lowerAge = rank + 4;
         return lowerAge != 18 ? lowerAge + 1 : 24;
     }
