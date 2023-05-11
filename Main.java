9b0f98e0432c7184143b02cac010b001fe2eea70
Kris Martin <28503716+Kris-Martin@users.noreply.github.com>
2023-05-11 23:47:03 +0930

    Refactor, add user input and menu

--- Main.java
+++ Main.java
@@ -2,65 +2,77 @@ package readability;
 
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.util.Arrays;
-import java.util.Scanner;
+import java.util.*;
 
 
 public class Main {
     public static void main(String[] args) {
-
         // args[0] for command line, "~/test1.txt" for test
         try (Scanner scanner = new Scanner(new File("/Users/kristymartin/test1.txt"))) {
             String input = scanner.nextLine().trim();
             Text text = new Text(input);
-            Readability readability = new Readability(text);
+            System.out.println(text.getStats());
 
-            String textStats =
-                    """
-                    Words: %s
-                    Word count: %d
-                    Sentences: %d
-                    Syllable count: %d
-                    Polysyllables: %d
-                    """.formatted(
-                            Arrays.toString(text.getWords()),
-                            text.getWordCount(),
-                            text.getSentenceCount(),
-                            text.getTotalSyllables(),
-                            text.getPolySyllables());
-            System.out.println(textStats);
+            String validSelections = "ARI, FK, SMOG, CL, ALL";
+            String selection = "none";
+            Scanner userInput = new Scanner(System.in);
 
-            double ari = readability.getARI();
-            int ariAgeLevel = Readability.getUpperAgeLevel(ari);
-            double fleshKincaid = readability.getFleschKincaid();
-            int fleshKincaidAgeLevel = Readability.getUpperAgeLevel(fleshKincaid);
-            double smog = readability.getSMOG();
-            int smogAgeLevel = Readability.getUpperAgeLevel(smog);
-            double colemanLiau = readability.getColemanLiau();
-            int colemanLiauAgeLevel = Readability.getUpperAgeLevel(colemanLiau);
-
-            String readabilityScores =
-                    """
-                    Automated Readability Index: %.2f (about %d-year-olds).
-                    Flesch–Kincaid readability tests: %.2f (about %d-year-olds).
-                    Simple Measure of Gobbledygook: %.2f (about %d-year-olds).
-                    Coleman–Liau index: %.2f (about %d-years-old).
-                    
-                    This text should be understood in average by %.2f-year-olds.
-                    """.formatted(
-                            ari,
-                            ariAgeLevel,
-                            fleshKincaid,
-                            fleshKincaidAgeLevel,
-                            smog,
-                            smogAgeLevel,
-                            colemanLiau,
-                            colemanLiauAgeLevel,
-                            Readability.getAverageAgeLevel(ariAgeLevel, fleshKincaidAgeLevel, smogAgeLevel, colemanLiauAgeLevel));
-            System.out.println(readabilityScores);
+            while (!validSelections.contains(selection.toUpperCase())) {
+                System.out.print("Enter the score you want to calculate (ARI, FK, SMOG, CL, all): ");
+                selection = userInput.nextLine().trim().toUpperCase();
+                System.out.println();
+                getSelection(selection, text);
+            }
 
         } catch (FileNotFoundException e) {
             System.out.println(e.getMessage());
         }
+    }
+
+    private static void getSelection(String selection, Text text) {
+        switch (selection) {
+            case "ARI" -> {
+                double score = Indexes.getARI(text);
+                int age = Indexes.getUpperAgeLevel(score);
+                printOutput(Indexes.ARI.name(), score, age);
+            }
+            case "FK" -> {
+                double score = Indexes.getFleschKincaid(text);
+                int age = Indexes.getUpperAgeLevel(score);
+                printOutput("Flesch–Kincaid readability tests", score, age);
+            }
+            case "SMOG" -> {
+                double score = Indexes.getSMOG(text);
+                int age = Indexes.getUpperAgeLevel(score);
+               printOutput("Simple Measure of Gobbledygook", score, age);
+            }
+            case "CL" -> {
+                double score = Indexes.getColemanLiau(text);
+                int age = Indexes.getUpperAgeLevel(score);
+                printOutput("Coleman–Liau index", score, age);
+            }
+            case "ALL" -> {
+                Map<String, Double> indexes = Map.of(
+                        Indexes.ARI.getName(), Indexes.getARI(text),
+                        Indexes.FK.getName(), Indexes.getFleschKincaid(text),
+                        Indexes.SMOG.getName(), Indexes.getSMOG(text),
+                        Indexes.CL.getName(), Indexes.getColemanLiau(text));
+
+                List<Integer> ageLevels = new ArrayList<>();
+                indexes.forEach((name, score) -> {
+                    int age = Indexes.getUpperAgeLevel(score);
+                    ageLevels.add(age);
+                    printOutput(name, score, age);
+                });
+
+                System.out.printf("%nThis text should be understood in average by %.2f-year-olds.%n",
+                        Indexes.getAverageAgeLevel(ageLevels));
+            }
+            default -> System.out.println("Error! Invalid selection. Please try again.");
+        }
+    }
+
+    public static void printOutput(String name, double score, int age) {
+        System.out.printf("%s: %.2f (about %d-year-olds).%n",  name, score, age);
     }
 }
